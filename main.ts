814c88c605da5c5e90c3caf0b555c4171dc075c4
0phoff <0phoff@users.noreply.github.com>
2022-11-03 16:17:09 +0100

    Added commands

--- main.ts
+++ main.ts
@@ -1,137 +1,80 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+import { Plugin } from 'obsidian';
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
 	async onload() {
-		await this.loadSettings();
+		console.log('SideToggleCommand Plugin loaded.');
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// Left Sidebar
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'STC-show-left-sidebar',
+			name: 'Show left sidebar',
+			callback: () => this.sidebarVisibility('left', true),
+		});
+
+		this.addCommand({
+			id: 'STC-hide-left-sidebar',
+			name: 'Hide left sidebar',
+			callback: () => this.sidebarVisibility('left', false),
+		});
+
+		// Right Sidebar
+		this.addCommand({
+			id: 'STC-show-right-sidebar',
+			name: 'Show right sidebar',
+			callback: () => this.sidebarVisibility('right', true),
+		});
+
+		this.addCommand({
+			id: 'STC-hide-right-sidebar',
+			name: 'Hide right sidebar',
+			callback: () => this.sidebarVisibility('right', false),
+		});
+
+		// Both Sidebars
+		this.addCommand({
+			id: 'STC-show-both-sidebars',
+			name: 'Show both sidebars',
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
+				this.sidebarVisibility('left', true);
+				this.sidebarVisibility('right', true);
+			},
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+		this.addCommand({
+			id: 'STC-hide-both-sidebars',
+			name: 'Hide both sidebars',
+			callback: () => {
+				this.sidebarVisibility('left', false);
+				this.sidebarVisibility('right', false);
+			},
+		});
+
+		this.addCommand({
+			id: 'STC-toggle-both-sidebars',
+			name: 'Toggle both sidebars',
+			callback: () => {
+				if (this.app.workspace.leftSplit.collapsed && this.app.workspace.rightSplit.collapsed) {
+					this.app.workspace.leftSplit.expand();
+					this.app.workspace.rightSplit.expand();
+				} else {
+					this.app.workspace.leftSplit.collapse();
+					this.app.workspace.rightSplit.collapse();
 				}
-			}
+			},
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-
+	async onunload() {
+		console.log('SideToggleCommand Plugin unloaded.');
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+	sidebarVisibility(side: 'left' | 'right', visible: boolean): void {
+		const sidebar = side === 'left' ? this.app.workspace.leftSplit : this.app.workspace.rightSplit;
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		if (sidebar.collapsed && visible) {
+			sidebar.expand();
+		} else if (!sidebar.collapsed && !visible) {
+			sidebar.collapse();
+		}
 	}
 }
